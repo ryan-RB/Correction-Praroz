ce35ab941ec42aadf2eea67e6a9236719104263c
BOUGHRIET Ryan <ryanmixt.rb@gmail.com>
2022-04-07 13:46:33 +0200

    MaJ

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,32 @@
-document.addEventListener("DOMContentLoaded", e => {
-    console.log("JS Loaded")
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("JS Loaded.");
+    let warning = document.querySelector(".alert");
+    let email = document.querySelector(".email");
+    let mdp = document.querySelector(".password");
+    let btn = document.getElementById("connexion");
     
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        formActive();
+    });
+
+    let formActive = () => {
+
+        email = document.querySelector(".email").value;
+        mdp = document.querySelector(".password").value;
+
+        console.log(email , mdp);
+
+        if (email && mdp){
+            warning.classList.add("success");
+            warning.classList.remove("error");
+            warning.innerText = "Connexion réussie.";
+            localStorage.setItem("utilisateur", email);
+            sessionStorage.setItem("utilisateur", "idsession");
+        } else {
+            warning.classList.add("error");
+            warning.innerText = "Mail ou mot de passe incorrecte.";
+            warning.classList.remove("success");
+        }
+    };
 });
